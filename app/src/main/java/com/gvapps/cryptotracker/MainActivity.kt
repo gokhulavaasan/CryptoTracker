cccdee143d00fb7a56a6fe87230cc1fc963f17a6
gokhul <gokhulavaasan@gmail.com>
2024-10-23 21:48:43 +0300

    coin list item added

--- app/src/main/java/com/gvapps/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/gvapps/cryptotracker/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.gvapps.cryptotracker.ui.theme.CryptoTrackerTheme
+import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
